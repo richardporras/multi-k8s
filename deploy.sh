5cc79a47a46b3b57596d9ff6eb53972c484021dc
Ricardo Porras <rporras@sanitas.es>
2019-06-08 07:03:01 +0200

    fixed worker container name typo

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ docker push ricopopo92/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=ricopopo92/multi-server:$SHA
 kubectl set image deployments/client-deployment client=ricopopo92/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=ricopopo92/worker-client:$SHA
+kubectl set image deployments/worker-deployment worker=ricopopo92/multi-worker:$SHA
